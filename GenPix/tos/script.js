dbce43aff677177323abd17262a398c9dd13f56d
GenPiix <149683188+GenPiix@users.noreply.github.com>
2023-11-02 15:13:53 +0100

    Delete GenPix directory

--- GenPix/tos/script.js
+++
@@ -1,7 +0,0 @@
-let menuIcon = document.querySelector('#menu-icon');
-let nav = document.querySelector('.navbar');
-
-menuIcon.onclick = () =>{
-    menuIcon.classList.toggle('bx-x')
-    nav.classList.toggle('active')
-}
